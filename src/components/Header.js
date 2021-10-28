12d851ca9f5f829de18dc48c7dccf9126dc6581d
Beatriz Godoy <beatriz.godoy@osedea.com>
2021-10-28 12:55:11 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = (props) => {
+    return <h1 style={{ textAlign: 'center' }}>Welcome to the Osedea Coding Challenge!</h1>;
+};
+
+export default Header;
---
+++ src/views/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Here are a list of beers/breweries...</h2>
+            </div>
+        );
+    }
+}
